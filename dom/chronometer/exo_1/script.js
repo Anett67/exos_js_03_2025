1dd8a0ebf3a5cc2e2630f83facbcb4ab8cbb92e1
Anett David <davidanett@gmail.com>
2025-05-15 16:07:25 +0200

    exos DOM

---
+++ dom/chronometer/exo_1/script.js
@@ -0,0 +1,4 @@
+// Affiche le message de bienvenue dans le DOM après 2 secondes
+setTimeout(() => {
+    document.getElementById("welcome").textContent = "Bienvenue dans le monde du JavaScript !";
+}, 2000);
